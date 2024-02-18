ab193ed98ebe037a06a2d8394a98400ea159777f
Bharath1910 <av.bharath@proton.me>
2024-02-18 23:38:40 +0530

    templating done

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+
+class Hostel:
+    @staticmethod
+    def login() -> None:
+        pass
+
+    @staticmethod
+    def logout() -> None:
+        pass
+
+    @staticmethod
+    def generate_headers() -> dict:
+        pass
+
+class Campus:
+    @staticmethod
+    def login() -> None:
+        pass
+
+    @staticmethod
+    def logout() -> None:
+        pass
+
+    @staticmethod
+    def generate_headers() -> dict:
+        pass
+
+    @staticmethod
+    def fetch_magic() -> str:
+        pass
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
