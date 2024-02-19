616268b2dd42cb4f6815e2a3562858bc597b782e
Bharath1910 <av.bharath@proton.me>
2024-02-19 11:35:43 +0530

    completed login, and added config file

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
 import requests
+import json
+from typing import TypedDict
+from abc import ABC, abstractmethod
+from datetime import datetime
 
-class Hostel:
-    @staticmethod
-    def login() -> None:
+class Config(TypedDict):
+    username: str
+    password: str
+    hostel_endpoint: str
+    campus_endpoint: str
+    # TODO: Add on success and failure runners
+    # on_success: list[str]
+    # on_failure: list[str]
+
+# I really have to come up with a better name than Base :/
+class Base(ABC):
+    def __init__(self) -> None:
+        # TODO: Implement exception handling
+        self.config: Config = json.load(open("./config.json", "r"))
+    
+    @abstractmethod
+    def login(self) -> None:
         pass
 
-    @staticmethod
-    def logout() -> None:
+    @abstractmethod
+    def logout(self) -> None:
+        pass
+
+    @abstractmethod
+    def generate_headers() -> dict:
+        pass
+
+class Hostel(Base):        
+    def login(self) -> None:
+        url = self.config['hostel_endpoint']
+        headers = Hostel.generate_headers()
+        data = {
+            "mode": "191",
+            "username": self.config['username'],
+            "password": self.config['password'],
+            "a": str(int(datetime.now().timestamp()))
+        }
+
+        response = requests.post(url, headers=headers, data=data, verify=False)
+        print(response.status_code)
+        print(response.text)
+
+
+    def logout(self) -> None:
         pass
 
     @staticmethod
@@ -32,25 +73,14 @@ class Hostel:
             "Connection": "close"
         }
 
-class Campus:
-    @staticmethod
-    def login() -> None:
-        pass
-
-    @staticmethod
-    def logout() -> None:
-        pass
-
-    @staticmethod
-    def generate_headers() -> dict:
-        pass
-
+class Campus(Base):
     @staticmethod
     def fetch_magic() -> str:
         pass
 
 def main() -> None:
-    pass
+    hostel = Hostel()
+    hostel.login()
 
 if __name__ == "__main__":
     main()
